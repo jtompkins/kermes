2a7994e1232e0eb3ac3b26a5b6d853870709ae99
Joshua Tompkins <josh@joshtompkins.com>
2020-09-07 12:35:42 -0500

    Remove create table function

--- fetcher-service/repositories/user_repository.py
+++ fetcher-service/repositories/user_repository.py
@@ -2,9 +2,6 @@ class UserRepository:
     def __init__(self, adapter):
         self.adapter = adapter
 
-    def create_table(self):
-        return self.adapter.create_table()
-
     def get(self, key):
         return self.adapter.get(key)
 
