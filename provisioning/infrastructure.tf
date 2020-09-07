06310337af1c5c842f5aaf6e2f03d94fbbd7aaa7
Joshua Tompkins <josh@joshtompkins.com>
2020-09-07 12:05:11 -0500

    Initial terraform provisioning script

---
+++ provisioning/infrastructure.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+
+  endpoints {
+    dynamodb = "http://localhost:4566"
+  }
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name         = "users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "user_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+}
