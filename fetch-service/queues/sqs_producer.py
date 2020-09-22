486bca709d6f285bc535b5c7b5272af63f675b0d
Joshua Tompkins <josh@joshtompkins.com>
2020-09-21 20:23:48 -0500

    add sqs producer helper class

---
+++ fetch-service/queues/sqs_producer.py
@@ -0,0 +1,25 @@
+from typing import Dict, Optional
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+class SQSProducer:
+    def __init__(self, queue_name: str, message_group_id: str) -> None:
+        self.sqs = boto3.client("sqs", endpoint_url="http://localhost:4566")
+        self.queue_url: str = self.sqs.get_queue_url(QueueName=queue_name)["QueueUrl"]
+        self.message_group_id = message_group_id
+
+    def send_message(self, message: str) -> Optional[str]:
+        try:
+            response: Dict = self.sqs.send_message(
+                QueueUrl=self.queue_url, MessageBody=message, MessageGroupId=self.message_group_id
+            )
+
+            if "MessageId" in response:
+                return response["MessageId"]
+            else:
+                return None
+        except ClientError as err:
+            print(err)
+            return None
